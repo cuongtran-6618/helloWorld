5be374ca58734f863043776e1c8ee1fb2a042019
Cuong Tran <mailtocuong@gmail.com>
2023-10-01 23:46:21 +0700

    Add variable type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 import java.time.LocalDate
 
-fun main(args: Array<String>) {
+fun main() {
     println("Who are you?")
-    val readOnlyName = readln()
-    println("Hello, $readOnlyName");
+    val readOnlyName: String = readln()
+    println("Hello, $readOnlyName")
 
     println("What is the year you have born?")
-    val yearOfBirthday = readln()
+    val yearOfBirthday: UInt = readln().toUInt()
     println("You was born at: $yearOfBirthday, so you are ${LocalDate.now().year - yearOfBirthday.toInt()}")
 }
