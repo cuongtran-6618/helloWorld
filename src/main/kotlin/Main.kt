6ff0cf6a7830f94404b1027147ec87715259344e
Cuong Tran <mailtocuong@gmail.com>
2023-10-01 23:34:21 +0700

    vairable in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 fun main(args: Array<String>) {
     println("Who are you?")
-    val name = readln()
-    println("Hello, $name");
+    val readOnlyName = readln()
+    println("Hello, $readOnlyName");
+
+    println("What year is it?")
+    var mutableName = readln()
+    println("Hello, $mutableName");
+
+    println("What is the year you have born?")
+    mutableName = readln()
+    println("You was born at, $mutableName");
 }
