cff86d21b22c60b758e470c059c5dc4f36875fa7
Cuong Tran <mailtocuong@gmail.com>
2023-10-11 22:56:37 +0700

    separate function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 import java.time.LocalDate
 
 fun main() {
+    calculateUserAge();
+    val countriesILived: MutableList<String> = mutableListOf("Vietnam", "Germany", "Finland")
+}
+
+fun calculateUserAge(): Unit {
     println("Who are you?")
+
     val readOnlyName: String = readln()
     println("Hello, $readOnlyName")
 
     println("What is the year you have born?")
     val yearOfBirthday: UInt = readln().toUInt()
+
     println("You was born at: $yearOfBirthday, so you are ${LocalDate.now().year - yearOfBirthday.toInt()}")
 }
